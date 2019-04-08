3d5ca6b188e4052605b9bb03350a3b749c886a9a
vl <valerie.lafaiteur@le-campus-numerique.fr>
2019-04-08 14:43:43 +0200

    Exercice 1 & 2

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,1 +1,10 @@
+/*Différence entre Javascript et Jquery : 
+JavaScript est un langage de programmation et jQuery est une bibliothèque/framework écrit en JavaScript open-source afin de simplifier et de condenser le code
+Un des gros avantages de jQuery par rapport au JavaScript pur est la notion de sélecteur qui permet d'identifier plus facilement l'élément sur lequel on désire interagir.*/
+
+
+
 console.log('exercice 1');
+$(document).ready(function () {
+    console.log('Je peux maintenant écrire du code JQuery');
+});
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,32 @@
 console.log('exercice 2');
+$(document).ready(function () {
+    $('h4').hide(); // je sélectionne le titre "John's Blog"
+    console.log($('h4'));
 
+    $('.sidenav'); //tous les éléments du sidemenu de gauche.
+    console.log($('.sidenav'));
+
+    $('footer'); //Sélectionner le footer.
+    console.log($('footer'));
+
+    $('input'); //Sélectionner l'élément qui permet de faire la barre de recherche.
+    console.log($('input'));
+    $('.glyphicon'); //Sélectionner tous les glyphicons de la page.
+    console.log($('.glyphicon'));
+
+    $('.label.label-success');  //Sélectionner tous les labels verts.
+    console.log($('.label.label-success'));
+
+    $('p.intro'); //Sélectionner tous les paragraphes qui ont comme classe 'intro'
+    console.log( $('p.intro'));
+
+    $('.glyphicon-time'); //Sélectionner toutes les images avec toutes les icones dont la classe est 'glyphicon-time'
+    console.log($('.glyphicon-time'));
+
+    $('#section1,.active');//Sélectionner le premier élément du sidemenu "home"
+    console.log($('#section1,.active'));
+
+    $('p:even');
+    console.log($('p:even'));
+
+});
