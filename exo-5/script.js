9d83ac5ea6bf4c2052a3db93ac5394b925f60f4d
vl <valerie.lafaiteur@le-campus-numerique.fr>
2019-04-11 16:32:01 +0200

    Exercice 5 OK

--- exo-5/script.js
+++ exo-5/script.js
@@ -1,1 +1,106 @@
 console.log("exercice 5");
+
+$(document).ready(function () {
+
+
+
+    $("img").on('click', function () {
+        $("#computerChoice").remove();
+        $("#userChoice").remove();
+        $("#result").remove();
+
+        var userChoice = $(this);
+        var computerChoice = Math.round(Math.random() * 2);
+
+
+        getUserChoice(userChoice);
+
+        if (computerChoice == 0) {
+            computerChoice = "pierre";
+
+        }
+        else if (computerChoice == 1) {
+            computerChoice = "feuille";
+
+        }
+        else if (computerChoice == 2) {
+            computerChoice = "ciseaux";
+
+        }
+
+        $(".images").append("<div id=computerChoice> Choix du computer : " + computerChoice + "</div>");
+        console.log("Choix de l'ordi : " + computerChoice);
+
+        determineWinner(userChoice.attr("id"), computerChoice);
+
+
+    })
+
+    function getUserChoice(userChoice) {
+
+        console.log("Vous avez choisi: " + userChoice.attr('id'));
+        $(".images").append("<div id=userChoice> Vous avez choisi : " + userChoice.attr("id") + "</div>");
+    }
+
+    function getComputerChoice(computerChoice) {
+
+        if (computerChoice == 0) {
+            computerChoice = "pierre";
+
+        }
+        else if (computerChoice == 1) {
+            computerChoice = "feuille";
+
+        }
+        else if (computerChoice == 2) {
+            computerChoice = "ciseaux";
+
+        }
+        $(".images").append("<div id=computerChoice> Choix du computer : " + computerChoice + "</div>");
+        console.log("Choix de l'ordi : " + computerChoice);
+    }
+
+
+    function determineWinner(userChoice, computerChoice, result) {
+
+
+
+        if (userChoice == computerChoice) {
+            console.log('Egalité');
+            result = "EGALITE";
+
+        }
+
+        else if (userChoice == 'pierre' && computerChoice == 'ciseaux') {
+            console.log('gagné');
+            result = "GAGNE";
+        }
+        else if (userChoice == 'feuille' && computerChoice == 'pierre') {
+            console.log('gagné');
+            result = "GAGNE";
+        }
+        else if (userChoice == 'ciseaux' && computerChoice == 'feuille') {
+            console.log('gagné');
+            result = "GAGNE";
+        }
+
+        else {
+            console.log('perdu');
+            result = "PERDU";
+        }
+
+        if (result === 'GAGNE') {
+            $(".images").append("<div id=result> Résultat : " + result + "</div>");
+            $("#result").css("color", "green");
+        }
+        if (result === 'EGALITE') {
+            $(".images").append("<div id=result> Résultat : " + result + "</div>");
+            $("#result").css("color", "grey");
+        }
+        if (result === 'PERDU') {
+            $(".images").append("<div id=result> Résultat : " + result + "</div>");
+            $("#result").css("color", "red");
+        }
+    }
+
+});
