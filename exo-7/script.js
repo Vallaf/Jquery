f835e7fad14f6b7f840a654907699dcb9d622a30
vl <valerie.lafaiteur@le-campus-numerique.fr>
2019-04-15 16:52:30 +0200

    Exercice 7 1/2/3

--- exo-7/script.js
+++ exo-7/script.js
@@ -1,4 +1,35 @@
 console.log("exercice 7");
+var jsonDatas = getData();
+$(document).ready(function () {
+
+  addLineToTable();
+
+  
+
+});
+
+function addLineToTable() {
+  let $newTbody = $("tbody");
+  $newTbody.empty();
+
+  jsonDatas.forEach(function (jsonData) {
+    let $tr = $("<tr>");
+    createTdLine($tr, jsonData.id);
+    createTdLine($tr, jsonData.name.first);
+    createTdLine($tr, jsonData.name.last);
+    createTdLine($tr, jsonData.email);
+    createTdLine($tr, jsonData.phone);
+    $newTbody.append($tr);
+  });
+
+}
+
+function createTdLine($tr, data) {
+  let $td = $("<td>");
+  $td.text(data);
+  $tr.append($td);
+
+}
 
 function getData() {
   return data; // data is defined in DATA.js file
